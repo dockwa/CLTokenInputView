3591dc09543a7e0014d4b94b5b85a014dfe5a66e
Christian Hatch <christianhatch@gmail.com>
2022-09-06 20:25:08 -0400

    try explicit header

---
+++ CLTokenInputView/CLTokenInputView/include/CLTokenInputView+BridgingHeader.h
@@ -0,0 +1,9 @@
+#ifndef CLTokenInputView+BridgingHeader_h
+#define CLTokenInputView+BridgingHeader_h
+
+#import "CLToken.h"
+#import "CLTokenView.h"
+#import "CLTokenInputView.h"
+#import "CLBackspaceDetectingTextField.h"
+
+#endif /* Header_h */
