1c74000dcd5deafb8fe7f230d0e53e42d903aed7
Christian Hatch <christianhatch@gmail.com>
2023-10-10 15:12:56 -0400

    Silence Warnings (#3)
    * add modulemap
    
    * try explicit header
    
    * fix name?
    
    * Extra tokens at end of #ifndef directive
    
    * bump podspec

---
+++ CLTokenInputView/CLTokenInputView/include/CLTokenInputView+BridgingHeader.h
@@ -0,0 +1,9 @@
+#ifndef CLTokenInputView_CLTokenInputView
+#define CLTokenInputView_CLTokenInputView
+
+#import "CLToken.h"
+#import "CLTokenView.h"
+#import "CLTokenInputView.h"
+#import "CLBackspaceDetectingTextField.h"
+
+#endif /* Header_h */
